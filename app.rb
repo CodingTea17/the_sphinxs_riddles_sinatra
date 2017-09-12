8c596c40430f716336eb8fbf6a2f7bf411aca230
Dan Kiss and Dawson Mortenson <dawson@>
2017-09-12 09:43:30 -0700

    select a random riddle question

--- app.rb
+++ app.rb
@@ -4,15 +4,17 @@ also_reload('lib/**/*.rb')
 require('./lib/sphinx')
 
 get('/') do
+  new_riddle = Riddle.new
+  @the_page_riddle = new_riddle.this_riddle.keys.first
   erb(:index)
 end
 
-post('/output') do
-  @a1 = params.fetch("q1")
-  @a2 = params.fetch("q2")
-  @a3 = params.fetch("q3")
-  # the_riddle = Riddle.new(@a1, @a2, @a3)
-  @riddle_me_this = Riddle.new(@a1, @a2, @a3).sphinx_thinking()
-  erb(:output)
-
-end
+# post('/output') do
+#   @a1 = params.fetch("q1")
+#   @a2 = params.fetch("q2")
+#   @a3 = params.fetch("q3")
+#   # the_riddle = Riddle.new(@a1, @a2, @a3)
+#   @riddle_me_this = Riddle.new(@a1, @a2, @a3).sphinx_thinking()
+#   erb(:output)
+#
+# end
--- lib/sphinx.rb
+++ lib/sphinx.rb
@@ -1,16 +1,19 @@
 class Riddle
+  attr_reader(:this_riddle)
   def initialize()
-    @answer1 = answer1.downcase
-    @answer2 = answer2.downcase
-    @answer3 = answer3.downcase
-    @random_number = 1+ rand(3)
+    riddles = [{"Poor people have it. Rich people need it. If you eat it you die.": "nothing"},{"What has 4 eyes but can’t see?": "mississippi"},{"What travels around the world but stays in one spot?": "stamp"}]
+    @this_riddle = riddles[rand(3)]
   end
 
-  def sphinx_thinking()
-    if ((@answer1 == "nothing") and (@answer2 == "mississippi") and (@answer3 == "stamp"))
-      return "Enter Dude"
-    else
-      return "You die now, release the dragon!"
-    end
-  end
+#   def sphinx_thinking()
+#     if ((@answer1 == "nothing") and (@answer2 == "mississippi") and (@answer3 == "stamp"))
+#       return "Enter Dude"
+#     else
+#       return "You die now, release the dragon!"
+#     end
+#   end
 end
+#
+# @answer1 = answer1.downcase
+# @answer2 = answer2.downcase
+# @answer3 = answer3.downcase
